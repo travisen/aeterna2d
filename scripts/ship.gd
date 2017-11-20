341a834cc78b712f8de9ea4abec0241b0283e966
travisen <travis.nykaza@gmail.com>
2017-11-19 21:44:52 -0500

    add initial project assets and code

---
+++ scripts/ship.gd
@@ -0,0 +1,23 @@
+extends Sprite
+
+var speed = 500
+var vel = Vector2()
+
+
+func _ready():
+    set_process(true)
+
+
+func _process(delta):
+    if Input.is_key_pressed(KEY_RIGHT):
+        vel = Vector2(speed, 0)
+    elif Input.is_key_pressed(KEY_LEFT):
+        vel = Vector2(-speed, 0)
+    elif Input.is_key_pressed(KEY_UP):
+        vel = Vector2(0, -speed)
+    elif Input.is_key_pressed(KEY_DOWN):
+        vel = Vector2(0, speed)
+    else:
+        vel = Vector2(0, 0)
+
+    set_pos(get_pos() + vel * delta)

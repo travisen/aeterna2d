e0d9b68d3f8c68aeecc49ee9f854ab58cac16ab8
travisen <travis.nykaza@gmail.com>
2017-11-30 22:12:45 -0500

    alter ship movement, start parallax background

---
+++ ScrollingBackground.gd
@@ -0,0 +1,14 @@
+extends ParallaxLayer
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	set_process(true)
+	
+func _process(delta):
+	var curPos = get_pos()
+	curPos.x = curPos.x - 100 * delta
+	curPos.y = curPos.y - 100 * delta
+	set_pos(curPos)
--- scripts/ship.gd
+++ scripts/ship.gd
@@ -2,22 +2,28 @@ extends Sprite
 
 var speed = 500
 var vel = Vector2()
-
+#var rot = .05
+var turn = .05
+var rot = get_rot()
 
 func _ready():
-    set_process(true)
+	set_process(true)
 
 
 func _process(delta):
-    if Input.is_key_pressed(KEY_RIGHT):
-        vel = Vector2(speed, 0)
-    elif Input.is_key_pressed(KEY_LEFT):
-        vel = Vector2(-speed, 0)
-    elif Input.is_key_pressed(KEY_UP):
-        vel = Vector2(0, -speed)
-    elif Input.is_key_pressed(KEY_DOWN):
-        vel = Vector2(0, speed)
-    else:
-        vel = Vector2(0, 0)
+	if Input.is_key_pressed(KEY_RIGHT) || Input.is_key_pressed(KEY_D):
+		set_rot(get_rot() - turn)
+		print(get_rot())
+	if Input.is_key_pressed(KEY_LEFT) || Input.is_key_pressed(KEY_A):
+		set_rot(get_rot() + turn)
+		print(get_rot())
+	if Input.is_key_pressed(KEY_UP) || Input.is_key_pressed(KEY_W):
+		rot = get_rot()
+		vel = Vector2(sin(rot), cos(rot)) * -speed;
+		print (get_pos())
+	if Input.is_key_pressed(KEY_DOWN) || Input.is_key_pressed(KEY_S):
+		rot = get_rot()
+		vel = Vector2(sin(rot), cos(rot)) * speed;
+		print (get_pos())
 
-    set_pos(get_pos() + vel * delta)
+	set_pos(get_pos() + vel * delta)
